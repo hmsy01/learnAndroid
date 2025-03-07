a2cd740ca144a549893a08853e62e7dd8ade8f5d
YangShen <hmsy01@gmail.com>
2025-03-07 22:54:10 +0800

    learn: 	Adapter 	ListView

--- app/src/main/java/com/example/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/learnandroid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.learnandroid;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_linear_layout);
+        String[] str = {"A","B","C"};
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,str);
+
+        ListView listView1 = (ListView) findViewById(R.id.listview);
+        listView1.setAdapter(myAdapter);
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
